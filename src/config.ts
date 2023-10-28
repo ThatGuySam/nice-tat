0b0208174d178e796366b1791c125c315b3837fa
ThatGuySam <sam@sam.lc>
2023-10-28 04:21:24 -0500

    Add description

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,6 @@
 
 
-export const SITE_NAME = 'Nice Tat';
+export const SITE_NAME = 'Nice Tat'
+
+
+export const DESCRIPTION = 'Astro Site Template inspired by Casey Neistat’s ultra minimal personal site. '
