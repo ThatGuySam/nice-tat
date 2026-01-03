40b10f745f81b2109a417ee4d43570473c62c3af
ThatGuySam <sam@sam.lc>
2026-01-03 17:57:55 -0600

    feat: add sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  // Required for sitemap, canonical URLs, OG images
+  site: 'https://nice-tat.pages.dev',
+  integrations: [
+    tailwind(),
+    sitemap(),
+  ]
 });
